745622910a7be243193e466c7a1d62b02cafb1bf
Connor Leech <connorleech@gmail.com>
2014-10-02 12:07:30 -0700

    add tooltips/zipcodes to oak map

--- assets/js/drawOak.js
+++ assets/js/drawOak.js
@@ -1,7 +1,8 @@
 (function(){
+
 	var container = d3.select('#ward-map'),
-		width = 400,
-		height = 400;
+		width = 500,
+		height = 500;
 
 	var svg = container.append('svg').attr('width', width).attr('height', height);
 
@@ -12,13 +13,14 @@
 	var path = d3.geo.path()
 		.projection(projection);
 
-	// get data
+	// get topoJSON of Oakland zipcodes
 	d3.json('../data/oakzipstopo.json', function(err, oak){
 		if(err) return console.error(err);
+
 		var zips = topojson.feature(oak, oak.objects.oakzips);
 
+		// set up center
 		projection.scale(1).translate([0,0]);
-
 		var b = path.bounds(zips),
 	    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
 	    t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
@@ -28,13 +30,21 @@
 		  .scale(s)
 		  .translate(t);
 
-		svg.append('path')
-			.datum(zips)
-			.attr('d', path)
-			.attr("class", "land-boundary");
+		// init tooltip 
+		var tip = d3.tip().attr('class', 'd3-tip').html(function(d){ 
+			console.log()
+			return d.properties.ZIP; 
+		});
+		svg.call(tip);
 
-		console.log(oak.objects.oakzips)
-
+		svg.selectAll('path')
+			.data(zips.features)
+			.enter()
+				.append('path')
+				.attr('d', path)
+				.attr("class", "zipzone")
+				.on('mouseover', tip.show)
+				.on('mouseout', tip.hide)
 
 	});
 	
