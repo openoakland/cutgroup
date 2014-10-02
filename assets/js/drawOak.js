b1fc427ed6a8c59446ae959c85626936f3b790a4
Connor Leech <connorleech@gmail.com>
2014-10-02 10:44:12 -0700

    map of oakland, no boundaries

---
+++ assets/js/drawOak.js
@@ -0,0 +1,41 @@
+(function(){
+	var container = d3.select('#ward-map'),
+		width = 400,
+		height = 400;
+
+	var svg = container.append('svg').attr('width', width).attr('height', height);
+
+	// Create a unit projection
+	var projection = d3.geo.albers();
+
+	// Create path generator
+	var path = d3.geo.path()
+		.projection(projection);
+
+	// get data
+	d3.json('../data/oakzipstopo.json', function(err, oak){
+		if(err) return console.error(err);
+		var zips = topojson.feature(oak, oak.objects.oakzips);
+
+		projection.scale(1).translate([0,0]);
+
+		var b = path.bounds(zips),
+	    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+	    t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+	    // Center map on Oakland
+		projection
+		  .scale(s)
+		  .translate(t);
+
+		svg.append('path')
+			.datum(zips)
+			.attr('d', path)
+			.attr("class", "land-boundary");
+
+		console.log(oak.objects.oakzips)
+
+
+	});
+	
+})();
